9acf89f333013d38bc85f37cd338277fd34e31b2
Efrat1 <42644291+Efrat1@users.noreply.github.com>
2023-02-01 13:29:30 +0200

    Use dynamic configuration to new Memory pool utility (#82)
    - Use default configurations if dynamic configuration were not defined in
      bft_config.yaml
    - Link YAML lib to preprocessor

--- bftengine/src/preprocessor/PreProcessor.hpp
+++ bftengine/src/preprocessor/PreProcessor.hpp
@@ -301,6 +301,9 @@ class PreProcessor : public bftEngine::IExternalObject {
     return logger_;
   }
 
+  concordUtil::MultiSizeBufferPool::SubpoolsConfig calcSubpoolsConfig();
+  std::unique_ptr<concordUtil::MultiSizeBufferPool> createbufferPool();
+
  private:
   const uint32_t MAX_MSGS = 10000;
   const uint32_t WAIT_TIMEOUT_MILLI = 100;
@@ -335,7 +338,7 @@ class PreProcessor : public bftEngine::IExternalObject {
   // engine supported size is kMaxSubpoolBuffersize_ - responseSizeInternalOverhead_
   static constexpr uint32_t kMaxSubpoolBuffersize_{1UL << 25};
   // Must be sorted in an ascending order, by bufferSize
-  const std::vector<concordUtil::MultiSizeBufferPool::SubpoolConfig> subpoolsConfig_;
+  const concordUtil::MultiSizeBufferPool::SubpoolsConfig subpoolsConfig_;
   std::unique_ptr<concordUtil::MultiSizeBufferPool> bufferPool_;
 
   concordMetrics::Component metricsComponent_;
